f3d273f757e8413e8f97d668f57e1f923d5f910d
Sergey V. Kior <svkior@gmail.com>
2016-05-09 15:32:58 +0300

    Initial

---
+++ reverse_test.go
@@ -0,0 +1,22 @@
+package stringutil
+
+import (
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, World", "dlroW ,olleH"},
+		{"123", "321"},
+		{"", ""},
+	}
+
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
